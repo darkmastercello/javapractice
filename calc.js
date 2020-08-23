30d584b7557c35f4a9c4ede03f686826a771c7af
Marcellus Githua <marcellusgithua@gmail.com>
2020-08-23 10:45:27 +0300

    add javascript

---
+++ calc.js
@@ -0,0 +1,6 @@
+var add = function (number1, number2) {
+    return number1 + number2;
+};
+var number1 = parseInt(prompt("enter number"))
+var number2 = parseInt(prompt("enter number"))
+alert(number1 + number2)
